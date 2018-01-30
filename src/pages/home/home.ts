cb46028f2056d34e7c397c175af507a5fa5b0bcf
p4cm4n972 <manuel.adele@gmail.com>
2018-01-30 10:44:53 +0100

    add toast

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LoadingController } from 'ionic-angular';
 import { RestProvider } from '../../providers/rest/rest';
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
 
 
 
@@ -11,7 +12,7 @@ import { RestProvider } from '../../providers/rest/rest';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, private rest: RestProvider) {
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, private rest: RestProvider, private toastCtrl: ToastController) {
 
   }
   initialisation() {
@@ -23,8 +24,19 @@ export class HomePage {
       console.log('response');
       setTimeout(() => {
         loader.dismiss();
+        let toast = this.toastCtrl.create({
+          message: 'Connection server successfully',
+          duration: 3000,
+          position: 'top',
+          cssClass: 'text-center'
+        });
+        toast.onDidDismiss(() => {
+          console.log('Dismissed toast');
+        });
 
+        toast.present();
       }, 5000);
+
     })
   }
   ionViewDidLoad() {
--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -9,13 +9,13 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class RestProvider {
-
+//private uri = 'http://10.1.1.77:9010/ws/supervision'
   constructor(public http: HttpClient) {
     console.log('RestProvider Provider');
   }
 
   initialisation() {
-    return this.http.get('http://10.1.1.77:9010/ws/supervision')
+    return this.http.get('http://localhost:8080/api/tables')
   }
 }
 
