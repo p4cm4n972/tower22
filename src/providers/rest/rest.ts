f0536ec5a64819db4b2f7832fb2a734ffac5c451
p4cm4n972 <manuel.adele@gmail.com>
2018-02-05 13:33:23 +0100

    access allow

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http/src/headers';
 
 /*
   Generated class for the RestProvider provider.
@@ -9,13 +10,19 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class RestProvider {
-private uri = 'http://localhost:9010/ws/heartbeat'
+private uri = 'http://localhost:9010/ws/heartbeat';
+private httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin':'http://localhost:9010'
+  })
+};
   constructor(public http: HttpClient) {
     console.log('RestProvider Provider');
   }
 
   initialisation() {
-    return this.http.get(this.uri)
+    return this.http.get(this.uri, this.httpOptions)
   }
 }
 
