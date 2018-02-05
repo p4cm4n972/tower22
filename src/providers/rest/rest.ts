9ec8235e376d9abeddfdd747044ec6ee7f9f3073
p4cm4n972 <manuel.adele@gmail.com>
2018-02-05 13:48:46 +0100

    'httpHeaders'

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http/src/headers';
+import { HttpHeaders } from '@angular/common/http';
 
 /*
   Generated class for the RestProvider provider.
