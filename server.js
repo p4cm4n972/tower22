01e4ef111420b868bc9b968ab368e1aabb0ab355
p4cm4n972 <manuel.adele@gmail.com>
2018-02-26 14:55:26 +0100

    settings payment

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
 });
 
 app.all('/', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost");
+  res.header("Access-Control-Allow-Origin", "http://localhost:9010");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
