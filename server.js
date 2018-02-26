919d280ef06c4dc32a1697ec8ad1c9987fbfe4d6
p4cm4n972 <manuel.adele@gmail.com>
2018-02-26 15:04:21 +0100

    settings payment

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://10.1.1.128/9010");
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 app.all('/', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:9010");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
