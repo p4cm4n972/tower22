e7d61dad56c9555dd74821f8aa7b379422cb0be2
p4cm4n972 <manuel.adele@gmail.com>
2018-02-26 14:36:38 +0100

    settings payment

--- src/providers/cart/cart.ts
+++ src/providers/cart/cart.ts
@@ -23,7 +23,7 @@ export class CartProvider {
   checkOut(invoice) {
     console.log(invoice);
     return this.http
-      .post("http://localhost:9010/ws/payment", invoice);
+      .post("http://localhost:9010/ws/payment", {TransactionNumber : invoice.TransactionNUmber, AmountToPay: invoice.AmountToPay});
   }
   dataticket() {
     console.log("Print Ticket");
