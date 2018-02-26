d700fd9d33ed4149d71f5aecea5d79b7bbc166e1
p4cm4n972 <manuel.adele@gmail.com>
2018-02-26 13:34:34 +0100

    settings payment

--- src/providers/cart/cart.ts
+++ src/providers/cart/cart.ts
@@ -1,9 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import * as socketIo from 'socket.io-client';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import * as socketIo from "socket.io-client";
 
-import { Socket } from '../../app/ws';
-
+import { Socket } from "../../app/ws";
 
 /*
   Generated class for the CartProvider provider.
@@ -16,27 +15,25 @@ export class CartProvider {
   cart: any[] = [];
   private socket: Socket;
 
-  
-  
-  
   constructor(public http: HttpClient) {
-    console.log('Hello CartProvider Provider');
+    console.log("Hello CartProvider Provider");
   }
-  
+
   checkOut(invoice) {
     console.log(invoice);
-    this.socket = socketIo('http://10.1.1.111:5000');
-    this.socket.emit('invoice',invoice);
-    console.log('OOOOOOOOOOOOOO');
-    return this.http.post('http://10.1.1.128:9010/ws/payment', invoice);
+    return this.http
+      .post("http://10.1.1.128:9010/ws/payment", invoice)
+      .subscribe(data => {
+        this.socket = socketIo("http://10.1.1.111:5000");
+        this.socket.emit("invoice", invoice);
+        console.log("OOOOOOOOOOOOOO");
+      });
   }
-dataticket() {
-  console.log('Print Ticket');
-  return this.http.post('http://10.1.1.128:9010/ws/dataticket',{HostId: "CIEME_01",TicketType:"AppTicket"})
+  dataticket() {
+    console.log("Print Ticket");
+    return this.http.post("http://10.1.1.128:9010/ws/dataticket", {
+      HostId: "CIEME_01",
+      TicketType: "AppTicket"
+    });
+  }
 }
-
-  
-  
-  
-}
-
