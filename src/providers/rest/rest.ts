b68e6198aa894c59c236dd0b8c13833a632d62f4
p4cm4n972 <manuel.adele@gmail.com>
2018-03-02 16:36:09 +0100

    save

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -19,41 +19,41 @@ export class RestProvider {
   private socket: Socket;
   observer: Observer<string>;
 
-  getStatus() : Observable<string> {
+  getStatus(): Observable<string> {
     console.log('GET')
     this.socket = socketIo('http://10.1.1.111:5000');
-    this.socket.on('clientdata', (res)=>{
-    this.observer.next(res.data)
-});
-    this.socket.on('receipt', (res)=>{
-    this.observer.next(res.data)
-});
-this.socket.on('CB', (res)=>{
-  this.observer.next(res.data)
-});
-return this.createObservable();
-}
-createObservable() : Observable<string> {
-  return new Observable<string>(observer => {
-    this.observer = observer;
-  });
-}
-private handleError(error) {
-  console.error('server error:', error);
-  if (error.error instanceof Error) {
+    this.socket.on('clientdata', (res) => {
+      this.observer.next(res.data)
+    });
+    this.socket.on('receipt', (res) => {
+      this.observer.next(res.data)
+    });
+    this.socket.on('CB', (res) => {
+      this.observer.next(res.data)
+    });
+    return this.createObservable();
+  }
+  createObservable(): Observable<string> {
+    return new Observable<string>(observer => {
+      this.observer = observer;
+    });
+  }
+  private handleError(error) {
+    console.error('server error:', error);
+    if (error.error instanceof Error) {
       let errMessage = error.error.message;
       return Observable.throw(errMessage);
+    }
+    return Observable.throw(error || 'Socket.io server error');
   }
-  return Observable.throw(error || 'Socket.io server error');
-}
 
 
 
 
   constructor(public http: HttpClient) {
     console.log('RestProvider Provider');
-    
-  }
 
   }
+
+}
 
